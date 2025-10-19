1f751633205bc5e054fe85e0be72b7e5cef0f007
sinchana18naik <sinchananaik112005@gmail.com>
2025-10-19 23:38:04 +0530

    save

--- simple_intrest.py
+++ simple_intrest.py
@@ -2,4 +2,6 @@ p=float(input("enter principal amount:))
 t=float(input("enter the time period :))
 r=float(input("enter rate of intrest:))
 si=(p*t*r)/100
+ci=p*((1+r/100)**t)-p
+print(f"compound intrest:{ci}")
 print(f"simple intrest: {si})
